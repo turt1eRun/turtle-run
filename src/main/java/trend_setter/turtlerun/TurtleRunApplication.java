f448eb9072c7cb8d9e6b6f2a169551b16af2b5b2
hyezuu <duudu0704@gmail.com>
2024-10-28 09:28:43 +0900

    init

---
+++ src/main/java/trend_setter/turtlerun/TurtleRunApplication.java
@@ -0,0 +1,13 @@
+package trend_setter.turtlerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurtleRunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/trend_setter/turtlerun/TurtleRunApplicationTests.java
@@ -0,0 +1,13 @@
+package trend_setter.turtlerun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleRunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
