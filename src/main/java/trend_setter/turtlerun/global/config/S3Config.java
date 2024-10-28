b3254db45f9b32cf20d3df3e3b064b8ce3648da6
hyezuu <duudu0704@gmail.com>
2024-10-28 17:51:43 +0900

    chore: s3 설정 수정

--- src/main/java/trend_setter/turtlerun/global/config/S3Config.java
+++ src/main/java/trend_setter/turtlerun/global/config/S3Config.java
@@ -10,11 +10,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
     private String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${spring.cloud.aws.credentials.secret-key}")
     private String secretKey;
-    @Value("${cloud.aws.region.static}")
+    @Value("${spring.cloud.aws.region.static}")
     private String region;
 
     @Bean
