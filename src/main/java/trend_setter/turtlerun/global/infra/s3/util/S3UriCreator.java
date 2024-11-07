fdf45f934beb9d0e00d372cafd59cf24ab10f001
hyezuu <duudu0704@gmail.com>
2024-11-07 16:21:41 +0900

    feat: s3 uri 생성 유틸클래스 추가

---
+++ src/main/java/trend_setter/turtlerun/global/infra/s3/util/S3UriCreator.java
@@ -0,0 +1,34 @@
+package trend_setter.turtlerun.global.infra.s3.util;
+
+import jakarta.annotation.PostConstruct;
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class S3UriCreator {
+    private static String bucket;
+    private static String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String instanceBucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String instanceRegion;
+
+    @PostConstruct
+    private void init() {
+        bucket = instanceBucket;
+        region = instanceRegion;
+    }
+
+    public static String createUri(String filePath) {
+        return UriComponentsBuilder.newInstance()
+            .scheme("https")
+            .host(bucket + ".s3." + region + ".amazonaws.com")
+            .path(filePath)
+            .build()
+            .toUriString();
+    }
+}
