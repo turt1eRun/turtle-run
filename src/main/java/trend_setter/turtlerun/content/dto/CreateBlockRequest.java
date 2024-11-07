7f3b4c7bdd0cad7843fafa738d85b2d1324c9b9c
hyezuu <duudu0704@gmail.com>
2024-11-07 16:18:51 +0900

    refactor: 설명블럭 요청 dto 클래스명 변경, javadoc 추가

--- src/main/java/trend_setter/turtlerun/content/dto/CreateBlockRequest.java
+++ src/main/java/trend_setter/turtlerun/content/dto/CreateBlockRequest.java
@@ -1,15 +1,20 @@
 package trend_setter.turtlerun.content.dto;
 
 import jakarta.validation.constraints.NotNull;
-import org.springframework.web.multipart.MultipartFile;
 import trend_setter.turtlerun.global.error.code.ContentErrorCode;
 import trend_setter.turtlerun.global.error.exception.ContentException;
+/**
+ * 설명 블록 생성 요청 DTO
+ *
+ * @param text       텍스트 블록일 경우 내용
+ * @param descFileId 이미지 블록일 경우 파일 식별자
+ * @param orderNum   블록 순서
+ */
+public record CreateBlockRequest(String text, Long descFileId, @NotNull Integer orderNum) {
 
-public record DescriptionBlockRequest(String text, MultipartFile file, @NotNull Integer orderNum) {
-
-    public DescriptionBlockRequest {
+    public CreateBlockRequest {
         boolean hasText = text != null && !text.isBlank();
-        boolean hasFile = file != null && !file.isEmpty();
+        boolean hasFile = descFileId != null;
 
         if (hasText && hasFile) {
             throw new ContentException(ContentErrorCode.BLOCK_CONTENT_CONFLICT);
