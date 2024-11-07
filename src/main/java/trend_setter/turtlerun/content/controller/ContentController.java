61b94d7678f9c8b44ce1ca503a58ac7a59337d8d
hyezuu <duudu0704@gmail.com>
2024-11-07 16:23:37 +0900

    feat: ContentController 추가

---
+++ src/main/java/trend_setter/turtlerun/content/controller/ContentController.java
@@ -0,0 +1,56 @@
+package trend_setter.turtlerun.content.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import trend_setter.turtlerun.content.dto.CreateContentRequest;
+import trend_setter.turtlerun.content.dto.GetFileUploadResponse;
+import trend_setter.turtlerun.content.service.ContentService;
+import trend_setter.turtlerun.content.service.DescriptionService;
+import trend_setter.turtlerun.content.service.ThumbnailService;
+import trend_setter.turtlerun.content.service.VideoService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/contents")
+public class ContentController {
+
+    private final ContentService contentService;
+    private final VideoService videoService;
+    private final ThumbnailService thumbnailService;
+    private final DescriptionService descriptionService;
+
+    @PostMapping("/videos")
+    public GetFileUploadResponse uploadVideo(@AuthenticationPrincipal UserDetails user,
+        @RequestPart("video") MultipartFile video) {
+        return videoService.uploadVideo(video);
+    }
+
+    @PostMapping("/thumbnails")
+    public GetFileUploadResponse uploadThumbnail(@RequestPart("thumbnail") MultipartFile thumbnail) {
+        return thumbnailService.uploadThumbnail(thumbnail);
+    }
+
+    @PostMapping("/block-images")
+    public GetFileUploadResponse uploadBlockImage(@RequestPart("block-image") MultipartFile blockImage) {
+        return descriptionService.uploadDescriptionImage(blockImage);
+    }
+
+    @PostMapping
+    public void createContent(
+        @AuthenticationPrincipal UserDetails user,
+        @RequestBody @Valid CreateContentRequest request
+    ) {
+        contentService.createContent(user, request);
+    }
+
+
+
+}
