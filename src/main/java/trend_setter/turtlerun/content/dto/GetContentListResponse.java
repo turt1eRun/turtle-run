f157266d944eed24ac0dea2d318b213d5d5a4c0d
hyezuu <duudu0704@gmail.com>
2024-11-09 21:56:57 +0900

    fix: 목록 dto에 썸네일 추가

--- src/main/java/trend_setter/turtlerun/content/dto/GetContentListResponse.java
+++ src/main/java/trend_setter/turtlerun/content/dto/GetContentListResponse.java
@@ -2,18 +2,15 @@ package trend_setter.turtlerun.content.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 import java.time.LocalDateTime;
+import trend_setter.turtlerun.content.entity.ThumbnailFile;
 
-public record GetContentListResponse(Long id, String title, String creator, long views,
-                                     int duration, LocalDateTime createdAt) {
+public record GetContentListResponse(Long id, String title, String creator, long views, int duration,
+                                     GetFileResponse thumbnailFileResponse, LocalDateTime createdAt) {
 
     @QueryProjection
     public GetContentListResponse(Long id, String title, String creator, long views, int duration,
-        LocalDateTime createdAt) {
-        this.id = id;
-        this.title = title;
-        this.creator = creator;
-        this.views = views;
-        this.duration = duration;
-        this.createdAt = createdAt;
+        ThumbnailFile thumbnailFile, LocalDateTime createdAt) {
+
+        this(id, title, creator, views, duration, GetFileResponse.from(thumbnailFile), createdAt);
     }
 }
