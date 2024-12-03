c92a5ccdd340fa52345cafead313c85f783572ac
hyezuu <duudu0704@gmail.com>
2024-12-03 18:36:39 +0900

    feat: 파일 soft delete 필드 추가

--- src/main/java/trend_setter/turtlerun/content/entity/DescriptionFile.java
+++ src/main/java/trend_setter/turtlerun/content/entity/DescriptionFile.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,11 @@ public class DescriptionFile {
 
     private String fileName;
     private String filePath;
+    private LocalDateTime deletedAt;
+
+    public void delete(){
+        this.deletedAt = LocalDateTime.now();
+    }
 
     public DescriptionFile(String fileName, String filePath) {
         this.fileName = fileName;
--- src/main/java/trend_setter/turtlerun/content/entity/ThumbnailFile.java
+++ src/main/java/trend_setter/turtlerun/content/entity/ThumbnailFile.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +24,11 @@ public class ThumbnailFile {
 
     private String fileName;
     private String filePath;
+    private LocalDateTime deletedAt;
+
+    public void delete(){
+        this.deletedAt = LocalDateTime.now();
+    }
 
     @Builder(builderMethodName = "testBuilder")
     public ThumbnailFile(String fileName, String filePath) {
