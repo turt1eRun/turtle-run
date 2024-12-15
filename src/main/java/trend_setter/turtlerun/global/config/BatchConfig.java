eb387d166c1b12d02cb5b01a2c8f08b8826988f1
hyezuu <duudu0704@gmail.com>
2024-12-15 20:44:20 +0900

    feat: S3파일 삭제를위한 배치환경 구성.

--- src/main/java/trend_setter/turtlerun/global/config/BatchConfig.java
+++ src/main/java/trend_setter/turtlerun/global/config/BatchConfig.java
@@ -17,11 +17,14 @@ import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.PlatformTransactionManager;
-import trend_setter.turtlerun.content.entity.DescriptionFile;
 import trend_setter.turtlerun.content.repository.DescriptionFileRepository;
+import trend_setter.turtlerun.content.repository.ThumbnailFileRepository;
+import trend_setter.turtlerun.content.repository.VideoFileRepository;
 import trend_setter.turtlerun.global.error.exception.FileException;
+import trend_setter.turtlerun.global.infra.s3.entity.S3Deletable;
 import trend_setter.turtlerun.global.infra.s3.service.S3ImageUploader;
 
 @Slf4j
@@ -32,41 +35,50 @@ public class BatchConfig {
     private final JobRepository jobRepository;
     private final S3ImageUploader s3ImageUploader;
     private final DescriptionFileRepository descriptionFileRepository;
+    private final VideoFileRepository videoFileRepository;
+    private final ThumbnailFileRepository thumbnailFileRepository;
     private final PlatformTransactionManager platformTransactionManager;
     public static final int EXPIRATION_HOURS = 24;
 
-    @Bean
-    public Job deleteS3descriptionFileJob() {
 
-        return new JobBuilder("descriptionFileDeleteJob", jobRepository)
-            .start(deleteS3descriptionFileStep())
+    @Bean
+    public Job deleteS3FileJob() {
+
+        return new JobBuilder("deleteS3FileJob", jobRepository)
+            .start(createDeleteStep("description", descriptionFileRepository))
+            .next(createDeleteStep("video", videoFileRepository))
+            .next(createDeleteStep("thumbnail", thumbnailFileRepository))
             .build();
     }
 
-    @Bean
-    public Step deleteS3descriptionFileStep() {
-        return new StepBuilder("deleteS3descriptionFileStep", jobRepository)
-            .<DescriptionFile, DescriptionFile>chunk(100, platformTransactionManager)
-            .reader(descriptionFileReader())
-            .processor(descriptionFileProcessor())
-            .writer(descriptionFileWriter())
+
+    private  <T extends S3Deletable> Step createDeleteStep(String prefix, JpaRepository<T, Long> repository
+    ) {
+        return new StepBuilder(prefix + "FileDeleteStep", jobRepository)
+            .<T, T>chunk(100, platformTransactionManager)
+            .reader(createReader(prefix, repository))
+            .processor(createProcessor())
+            .writer(createWriter(repository))
             .build();
     }
 
-    @Bean
-    public RepositoryItemReader<DescriptionFile> descriptionFileReader() {
-        return new RepositoryItemReaderBuilder<DescriptionFile>()
-            .name("descriptionFileReader")
-            .repository(descriptionFileRepository)
+
+    private  <T extends S3Deletable> RepositoryItemReader<T> createReader(
+        String prefix,
+        JpaRepository<T, Long> repository
+    ) {
+        return new RepositoryItemReaderBuilder<T>()
+            .name(prefix + "FileReader")
+            .repository(repository)
             .methodName("findByDeletedAtBefore")
             .pageSize(100)
             .arguments(List.of(LocalDateTime.now().minusHours(EXPIRATION_HOURS)))
-            .sorts(Map.of("id", Sort.Direction.ASC))
+            .sorts(Map.of("id", Direction.ASC))
             .build();
     }
 
-    @Bean
-    public ItemProcessor<DescriptionFile, DescriptionFile> descriptionFileProcessor() {
+
+    private  <T extends S3Deletable> ItemProcessor<T, T> createProcessor() {
         return file -> {
             try {
                 s3ImageUploader.delete(file.getFilePath());
@@ -78,10 +90,11 @@ public class BatchConfig {
         };
     }
 
-    @Bean
-    public RepositoryItemWriter<DescriptionFile> descriptionFileWriter() {
-        return new RepositoryItemWriterBuilder<DescriptionFile>()
-            .repository(descriptionFileRepository)
+
+    private  <T extends S3Deletable> RepositoryItemWriter<T> createWriter(
+        JpaRepository<T, Long> repository) {
+        return new RepositoryItemWriterBuilder<T>()
+            .repository(repository)
             .methodName("delete")
             .build();
     }
