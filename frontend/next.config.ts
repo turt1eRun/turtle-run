c6e2b1973b4350d532d1aff1cf98e8e1820ec920
hyezuu <duudu0704@gmail.com>
2024-12-21 21:41:42 +0900

    feat: api 요청 redirect 위한 설정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,4 +8,16 @@ const nextConfig: NextConfig = {
   }
 };
 
+//개발환경에서만 설정
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // 프론트엔드에서 호출하는 경로
+        destination: 'http://localhost:8080/api/:path*', // 백엔드 서버 주소
+      },
+    ];
+  },
+};
+
 export default nextConfig;
