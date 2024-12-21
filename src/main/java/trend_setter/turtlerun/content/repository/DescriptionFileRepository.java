c7b6bf819456246d970f938d436bc839fc77e3b3
hyezuu <duudu0704@gmail.com>
2024-12-21 21:45:36 +0900

    fix: 원본파일이름 추출 로직 추가

--- src/main/java/trend_setter/turtlerun/content/repository/DescriptionFileRepository.java
+++ src/main/java/trend_setter/turtlerun/content/repository/DescriptionFileRepository.java
@@ -4,9 +4,19 @@ import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import trend_setter.turtlerun.content.entity.DescriptionFile;
 
 public interface DescriptionFileRepository extends JpaRepository<DescriptionFile, Long> {
 
-    Page<DescriptionFile> findByDeletedAtBefore(LocalDateTime localDateTime, Pageable pageable);
+    Page<DescriptionFile> findByDeletedAtBefore(LocalDateTime threshold, Pageable pageable);
+
+    @Query("SELECT d FROM DescriptionFile d "
+        + "WHERE d.createdAt < :threshold "
+        + "AND d.deletedAt IS NULL "
+        + "AND NOT EXISTS ("
+        + "    SELECT 1 FROM DescriptionBlock db WHERE db.descriptionFile= d"
+        + ")")
+    Page<DescriptionFile> findOrphanFiles(@Param("threshold")LocalDateTime threshold, Pageable pageable);
 }
--- src/main/java/trend_setter/turtlerun/content/repository/ThumbnailFileRepository.java
+++ src/main/java/trend_setter/turtlerun/content/repository/ThumbnailFileRepository.java
@@ -4,9 +4,19 @@ import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import trend_setter.turtlerun.content.entity.ThumbnailFile;
 
 public interface ThumbnailFileRepository extends JpaRepository<ThumbnailFile, Long> {
 
-    Page<ThumbnailFile> findByDeletedAtBefore(LocalDateTime localDateTime, Pageable pageable);
+    Page<ThumbnailFile> findByDeletedAtBefore(LocalDateTime threshold, Pageable pageable);
+
+    @Query("SELECT t FROM ThumbnailFile t "
+        + "WHERE t.createdAt < :threshold "
+        + "AND t.deletedAt IS NULL "
+        + "AND NOT EXISTS ("
+        + "    SELECT 1 FROM Content c WHERE c.thumbnail = t"
+        + ")")
+    Page<ThumbnailFile> findOrphanFiles(@Param("threshold")LocalDateTime threshold, Pageable pageable);
 }
